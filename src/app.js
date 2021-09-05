5640e947fa00cedeab91f020adaa6cac29b5240c
shanejix <shanejix@hotmail.com>
2021-09-05 22:50:40 +0800

    init

--- src/app.js
+++
@@ -1,9 +0,0 @@
-export const dva = {
-  config: {
-    onStateChange(state,a) {
-      if (localStorage) {
-        localStorage.setItem("cart", JSON.stringify(state.cart));
-      }
-    }
-  }
-};
